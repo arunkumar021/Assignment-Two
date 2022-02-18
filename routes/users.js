f3fd4d5f55dbeb419f801e6c2c9aa920b290e310
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-02-18 02:50:19 -0800

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import { createUser, getUserWithId , deleteUser , updateUserInfo , login} from '../controllers/users.js';
 
+
+const router = express.Router();
+
+var users = [];
+
+router.post('/login' , login);
+
+router.post('/' , createUser);
+
+router.get('/:id' , getUserWithId);
+
+router.delete('/:id' , deleteUser);
+
+router.patch('/:id' , updateUserInfo)
+
+export default router;
