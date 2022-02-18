2a446ee8b15ab3171934d17bbdafc94d8661c8e2
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-02-18 02:27:16 -0800

    Add files via upload

--- users.js
+++ users.js
@@ -1,92 +1,19 @@
-import bcrypt from 'bcrypt';
-
-   var users = [];
-   
-
-   export const login =  async (req , res) => {
-    const user = users.find((user) =>user.name === req.body.name);
-   if(user == null) {
-    return res.status(400).send("Cannot find user");
-    }
-    try {
-    if(await bcrypt.compare(req.body.password , user.password)) {
-        res.send('Success');
-        return true;
-    } else {
-        res.send('Not Allowed');
-    }
-   }catch{
-    res.status(500).send();
-  }
-   };
+import express from 'express';
+import { createUser, getUserWithId , deleteUser , updateUserInfo , login} from '../controllers/users.js';
 
 
-   export const createUser = async (req , res)=>{
-    try{
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password , 10)
-        // console.log(salt);
-        // console.log(hashPassword);
-   const user = {name : req.body.name , password : hashedPassword };
-   console.log(user);
-   users.push(user);
-   res.send(`User created in the database`);
-    }catch{
-        res.status(500).send();
-    }
-};
+const router = express.Router();
 
-  
-export const getUserWithId = (req , res) =>{
-    let result = login();
-    if(result === true) {
-    const { id } = req.params;
-    if((users.some((user) => id === user.id)) === false) {
-        res.status(400).send("Invalid ID!");               //Bad Request
-    }
-    else {
-        const foundUser = users.find((user)=> id === user.id);
-        res.send(`FirstName : ${foundUser.firstName}
-                  LastName : ${foundUser.lastName}
-                  Age : ${foundUser.age}
-                  ID : ${id}
-        `);
-    }
-    res.send(users);
-}
-}
+var users = [];
 
-export const deleteUser =  (req , res) => {
-    if((users.some((user) => id === user.id)) === false) {
-        res.status(400).send("Invalid ID!");
-    }
-    else {
-    users = users.filter((user) => id !== user.id);
-    res.send(`User with the id ${id} deleted from the database`);
-    }
-};
+router.post('/login' , login);
 
-export const updateUserInfo = (req , res) => {
-    const {id} = req.params;
+router.post('/' , createUser);
 
-    if((users.some((user) => id === user.id)) === false) {
-        res.status(400).send("Invalid ID!");
-    }
+router.get('/:id' , getUserWithId);
 
-    else {
-    const {firstName , lastName , age} = req.body;
+router.delete('/:id' , deleteUser);
 
-    const user = users.find((user)=> user.id === id);
+router.patch('/:id' , updateUserInfo)
 
-    if(firstName) {
-        user.firstName = firstName;
-    }
-    if(lastName) {
-        user.lastName = lastName;
-    }
-    if(age) {
-        user.age = age;
-    }
-    res.send(`user with the id  ${id} has been updated `)
-  }
-}
+export default router;
